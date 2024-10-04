d6348087112d63d3a86373bb8d9362953f3bd708
ARPCharlie <charlie@arp-lan.com>
2024-10-04 21:17:18 +0200

    Chore: prettify
    prettify all files using prettier

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/tailwind.css'],
+  css: ["~/assets/css/tailwind.css"],
 
   postcss: {
     plugins: {
@@ -10,5 +10,5 @@ export default defineNuxtConfig({
     },
   },
 
-  compatibilityDate: '2024-10-04',
-})
+  compatibilityDate: "2024-10-04",
+});
