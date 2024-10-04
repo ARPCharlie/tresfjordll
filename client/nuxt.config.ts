93db73af520f4f21fc9540fcc548a13ed9d538b6
ARPCharlie <charlie@arp-lan.com>
2024-10-04 21:11:52 +0200

    Chore: packages
    add packages

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -2,10 +2,13 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/tailwind.css'],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
+  compatibilityDate: '2024-10-04',
 })
